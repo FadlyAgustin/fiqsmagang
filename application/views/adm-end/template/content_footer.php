657bd6c09c2571fc2f6edb5a02a60caab92afd9a
fadly <fadlypku1@gmail.com>
2024-07-28 22:43:11 +0700

    Project fiqsmagang

---
+++ application/views/adm-end/template/content_footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer text-left small">
+    <div class="float-right d-none d-sm-inline">
+      <strong class="text-info">Fiqs | Internship</strong>
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
